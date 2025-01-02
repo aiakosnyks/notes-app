ab9f2fa12605e58e3435b3c3619871632f6b780b
aiakosnyks <aiakosnyks@outlook.com>
2025-01-03 00:28:11 +0300

    increaseByAmount reducer added

--- src/components/ButtonComponent.js
+++ src/components/ButtonComponent.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {StyleSheet, Button, View} from 'react-native';
+import {StyleSheet, Button, View, TextInput} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { useDispatch } from 'react-redux';
-import { decrement, increment } from '../redux/counterSlice';
+import { decrement, increment, incrementByAmount } from '../redux/counterSlice';
+import { useState } from 'react';
 
 const ButtonComponent = () => {
   const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0);
   return (
-      <View style={styles.noteContainer}>
-      <Button title="Increment" onPress={() => dispatch(increment())} />
-      <Button title="Decrement" onPress={() => dispatch(decrement())} />
+      <View style={styles.container}>
+      <TextInput 
+        onChangeText={(value) => setAmount(Number(value))} 
+        style={styles.emailText} 
+        placeholder="Enter amount" 
+        keyboardType="numeric" 
+      />      
+        <Button title="Increment" onPress={() => dispatch(increment())} />
+        <Button title="Decrement" onPress={() => dispatch(decrement())} />
+        <Button title="IncreaseByAmount" onPress={() => dispatch(incrementByAmount(amount))} /> 
       </View>
   )
 }
@@ -24,6 +33,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
+  emailText: {
+    backgroundColor: 'red',
+    alignContent: 'center',
+    justifyContent: 'center',
+  }
 });
 
 export default ButtonComponent;
